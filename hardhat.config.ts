baadc1d3aa53e6a9b08340a041fb572d1af40303
just-a-node <eye1717@gmail.com>
2023-03-14 23:54:40 -0600

    Add hardhat networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,21 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: {
+    mainnet: {
+      chainId: 1,
+      url: "https://cloudflare-eth.com",
+    },
+    goerli: {
+      chainId: 5,
+      url: "https://goerli.infura.io/v3/7672e2bf7cbe427e8cd25b0f1dde65cf",
+    },
+    optimism: {
+      chainId: 10,
+      url: "https://mainnet.optimism.io",
+    },
+  },
 };
 
 export default config;
